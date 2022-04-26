3517e3ac7baf15091dd0c90d0db0a81906cfa174
paulguillen190 <paulguillen190@gmail.com>
2022-04-26 12:01:28 -0500

    Changes in View CuerposGeometricoActivity.kt

--- app/src/main/java/devpaul/business/kidmath/operations/settings/details/segmentthree/CuerposGeometricoActivity.kt
+++ app/src/main/java/devpaul/business/kidmath/operations/settings/details/segmentthree/CuerposGeometricoActivity.kt
@@ -1,12 +1,35 @@
 package devpaul.business.kidmath.operations.settings.details.segmentthree
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import cn.pedant.SweetAlert.SweetAlertDialog
 import devpaul.business.kidmath.R
 
 class CuerposGeometricoActivity : AppCompatActivity() {
+
+    var textDialog : TextView? = null
+    var textBack : TextView? = null
+
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cuerpos_geometrico)
+
+        //desactivar rotacion pantalla
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        textDialog = findViewById(R.id.next)
+        textDialog?.setOnClickListener {
+            SweetAlertDialog(this).setTitleText("Segimos trabajando en este tema, gracias por tu comprensión").show()
+        }
+
+        textBack = findViewById(R.id.text_back)
+        textBack?.setOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
